77ee95b6fa17b508f00b3b212967addecacd0388
Vince Reuter <vince.reuter@gmail.com>
2024-05-27 17:03:06 +0200

    first-pass at full support for looptrace v0.5.0

--- looptrace_loci_vis/points_parser.py
+++ looptrace_loci_vis/points_parser.py
@@ -25,45 +25,55 @@ class MappingLike(Protocol):
 
 class PointsParser(Protocol, Generic[Input]):
 
-    def parse_all_qcpass(self, data: Input) -> list[PointRecord]: ...
+    @classmethod
+    def parse_all_qcpass(cls, data: Input) -> list[PointRecord]: ...
     
-    def parse_all_qcfail(self, data: Input) -> list[tuple[PointRecord, QCFailReasons]]: ...
+    @classmethod
+    def parse_all_qcfail(cls, data: Input) -> list[tuple[PointRecord, QCFailReasons]]: ...
 
 
 class IterativePointsParser(Generic[I1, I2], PointsParser[I1]):
     
-    def _gen_records(self, data: I1) -> Iterable[I2]: ...
+    @classmethod
+    def _gen_records(cls, data: I1) -> Iterable[I2]: ...
 
-    def _parse_single_qcpass_record(self, record: I2) -> PointRecord: ...
+    @classmethod
+    def _parse_single_qcpass_record(cls, record: I2) -> PointRecord: ...
 
-    def _parse_single_qcfail_record(self, record: I2) -> tuple[PointRecord, QCFailReasons]: ...
+    @classmethod
+    def _parse_single_qcfail_record(cls, record: I2) -> tuple[PointRecord, QCFailReasons]: ...
 
-    def parse_all_qcpass(self, data: I1) -> list[PointRecord]:
-        return [self._parse_single_qcpass_record(r) for r in self._gen_records(data)]
+    @classmethod
+    def parse_all_qcpass(cls, data: I1) -> list[PointRecord]:
+        return [cls._parse_single_qcpass_record(r) for r in cls._gen_records(data)]
     
-    def parse_all_qcfail(self, data: I1) -> list[tuple[PointRecord, QCFailReasons]]:
-        return [self._parse_single_qcfail_record(r) for r in self._gen_records(data)]
+    @classmethod
+    def parse_all_qcfail(cls, data: I1) -> list[tuple[PointRecord, QCFailReasons]]:
+        return [cls._parse_single_qcfail_record(r) for r in cls._gen_records(data)]
 
 
 class HeadedTraceTimePointParser(IterativePointsParser[PathLike, MappingLike]):
 
     TIME_INDEX_COLUMN = "timeIndex"
 
-    def _gen_records(self, data: PathLike) -> pd.DataFrame:
+    @classmethod
+    def _gen_records(cls, data: PathLike) -> pd.DataFrame:
         return pd.read_csv(data)
     
-    def _parse_single_qcpass_record(self, record: MappingLike) -> PointRecord:
+    @classmethod
+    def _parse_single_qcpass_record(cls, record: MappingLike) -> PointRecord:
         trace = TraceId(int(record["traceId"]))
-        timepoint = Timepoint(int(record[self.TIME_INDEX_COLUMN]))
+        timepoint = Timepoint(int(record[cls.TIME_INDEX_COLUMN]))
         z = float(record["z"])
         y = float(record["y"])
         x = float(record["x"])
         point = ImagePoint3D(z=z, y=y, x=x)
         return PointRecord(trace_id=trace, timepoint=timepoint, point=point)
 
-    def _parse_single_qcfail_record(self, record: MappingLike) -> tuple[PointRecord, QCFailReasons]:
+    @classmethod
+    def _parse_single_qcfail_record(cls, record: MappingLike) -> tuple[PointRecord, QCFailReasons]:
         """A fail record parses the same as a pass one, just with one additional field for QC fail reasons."""
-        pt_rec = self._parse_single_qcpass_record(record)
+        pt_rec = cls._parse_single_qcpass_record(record)
         fail_code = record["failCode"]
         return pt_rec, fail_code
 
@@ -85,33 +95,34 @@ class HeadlessTraceTimePointParser(IterativePointsParser[PathLike, CsvRow]):
             """Alias for the value of this enum member"""
             return self.value
 
-    def __init__(self) -> None:
-        super().__init__()
-        self._number_of_columns = sum(1 for _ in self.InputFileColumn)
+    _number_of_columns = sum(1 for _ in InputFileColumn)
 
-    def _parse_single_record(self, r: CsvRow, *, exp_len: int) -> PointRecord:
+    @classmethod
+    def _parse_single_record(cls, r: CsvRow, *, exp_len: int) -> PointRecord:
         if not isinstance(r, list):
             raise TypeError(f"Record to parse must be list, not {type(r).__name__}")
         if len(r) != exp_len:
             raise ValueError(f"Expected record of length {exp_len} but got {len(r)}: {r}")
-        trace = TraceId(int(r[self.InputFileColumn.TRACE.get]))
-        timepoint = Timepoint(int(r[self.InputFileColumn.TIMEPOINT.get]))
-        z = float(r[self.InputFileColumn.Z.get])
-        y = float(r[self.InputFileColumn.Y.get])
-        x = float(r[self.InputFileColumn.X.get])
+        trace = TraceId(int(r[cls.InputFileColumn.TRACE.get]))
+        timepoint = Timepoint(int(r[cls.InputFileColumn.TIMEPOINT.get]))
+        z = float(r[cls.InputFileColumn.Z.get])
+        y = float(r[cls.InputFileColumn.Y.get])
+        x = float(r[cls.InputFileColumn.X.get])
         point = ImagePoint3D(z=z, y=y, x=x)
         return PointRecord(trace_id=trace, timepoint=timepoint, point=point)
 
-    def _gen_records(self, data: PathLike) -> Iterable[CsvRow]:
+    @classmethod
+    def _gen_records(cls, data: PathLike) -> Iterable[CsvRow]:
         with open(data, newline="") as fh: # noqa: PTH123
             rows = list(csv.reader(fh))
         return rows
     
-    def _parse_single_qcpass_record(self, record: CsvRow) -> PointRecord:
-        return self._parse_single_record(record, exp_len=self._number_of_columns - 1)
-    
-    def _parse_single_qcfail_record(self, record: CsvRow) -> tuple[PointRecord, QCFailReasons]:
-        pt_rec = self._parse_single_record(record, exp_len=self._number_of_columns)
-        fail_code = record[self.InputFileColumn.QC.get]
-        return pt_rec, fail_code
+    @classmethod
+    def _parse_single_qcpass_record(cls, record: CsvRow) -> PointRecord:
+        return cls._parse_single_record(record, exp_len=cls._number_of_columns - 1)
 
+    @classmethod    
+    def _parse_single_qcfail_record(cls, record: CsvRow) -> tuple[PointRecord, QCFailReasons]:
+        pt_rec = cls._parse_single_record(record, exp_len=cls._number_of_columns)
+        fail_code = record[cls.InputFileColumn.QC.get]
+        return pt_rec, fail_code
